36f04f0729b9415cb797f1174dec11740c1805fd
Andrea <andy@andy>
2013-12-02 14:25:55 +0000

    few minor edits

--- js/personal.js
+++ js/personal.js
@@ -1,21 +1,20 @@
-$( document ).ready(function() {
-    var SIZE = 14;
-    var WIDTH = 3;
-    var HEIGHT = 2;
-    
-    function tile(){
-        var li = $("i[class^=fav-");
-        var left  = 0;
-        var top  = 0;
-        for(var i=0;i < HEIGHT; ++i){
-            for(var j= 0; j < WIDTH;++j){
-            	console.log(left," ", top);
-                li.eq(i * WIDTH + j).css('background-position-x', left);
-                li.eq(i * WIDTH + j).css('background-position-y', top);
-                left -= j * SIZE;
-            	top -= i * SIZE;
-            }
-        }
-    }
-    
+$(document).ready(function() {
+	var SIZE = 14;
+	var WIDTH = 3;
+	var HEIGHT = 2;
+
+	function tile() {
+		var li = $("i[class^=favicon");
+		var left = 0;
+		var top = 0;
+		for ( var i = 0; i < HEIGHT; ++i) {
+			for ( var j = 0; j < WIDTH; ++j) {
+				var curr = li.eq(j + WIDTH * i);
+				var y = left - SIZE * j;
+				var x = top - SIZE * i;
+				curr.css("background-position", y + "px " + x + "px");
+			}
+		}
+	}
+	tile();
 });
